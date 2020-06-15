ae2db71b8618813f94f56e6a0b13815f09101aaf
Matt Riddett <35952057+lazyninjacat@users.noreply.github.com>
2020-06-15 00:06:32 -0700

    flashcard tutorial

--- Assets/Scripts/VW_Scripts/VW_Flashcards.cs
+++ Assets/Scripts/VW_Scripts/VW_Flashcards.cs
@@ -28,9 +28,19 @@ public class VW_Flashcards : MonoBehaviour
     private List<Transform> availableWordList = new List<Transform>();
     private List<int> wordIdsToSave = new List<int>();
 
+
+    // TUTORIAL STUFF
+    [SerializeField] GameObject tutorialPanel1;
+    [SerializeField] GameObject helpButtonIconOn;
+    [SerializeField] GameObject helpButtonIconOff;
+
+    List<GameObject> tutorialObjects;
+
     void Start()
     {
         sceneName = SceneManager.GetActiveScene().name;
+
+       
 
         // Populate the add new scrollview
         MAS_PlayList tempMaster = (MAS_PlayList)COM_Director.GetMaster("MAS_PlayList");
@@ -46,6 +56,63 @@ public class VW_Flashcards : MonoBehaviour
             flash = JsonUtility.FromJson<DO_FlashCard>(controller.GetJsonByIndex(controller.GetActiveContextIndex()));
             ToggleAllToggles();
             PopulateBothViews();
+        }
+
+        tutorialObjects = GameObject.FindGameObjectsWithTag("tutorial").ToList();
+
+        foreach (GameObject obj in tutorialObjects)
+        {
+            obj.SetActive(false);
+        }
+
+        if (PlayerPrefs.GetInt("isTutorial") == 1)
+        {
+            tutorialPanel1.SetActive(true);
+            helpButtonIconOn.SetActive(false);
+            helpButtonIconOff.SetActive(true);
+        }
+        else
+        {
+            helpButtonIconOn.SetActive(true);
+            helpButtonIconOff.SetActive(false);
+        }
+
+
+    }
+
+    private void Update()
+    {
+        if (PlayerPrefs.GetInt("isTutorial") == 0)
+        {
+            foreach (GameObject obj in tutorialObjects)
+            {
+                obj.SetActive(false);
+            }
+        }
+    }
+
+    public void ToggleTutorialButton()
+    {
+        if (PlayerPrefs.GetInt("isTutorial") == 0)
+        {
+            PlayerPrefs.SetInt("isTutorial", 1);
+            foreach (GameObject obj in tutorialObjects)
+            {
+                obj.SetActive(false);
+            }
+            tutorialPanel1.SetActive(true);
+            helpButtonIconOn.SetActive(false);
+            helpButtonIconOff.SetActive(true);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("isTutorial", 0);
+            foreach (GameObject obj in tutorialObjects)
+            {
+                obj.SetActive(false);
+            }
+            helpButtonIconOn.SetActive(true);
+            helpButtonIconOff.SetActive(false);
         }
     }
 
